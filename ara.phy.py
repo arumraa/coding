987bc30f24fba040b5201137f54582eea599c6b3
arumraa <113223271+arumraa@users.noreply.github.com>
2022-09-10 10:31:14 +0700

    jbdbsjhaj

---
+++ ara.phy.py
@@ -0,0 +1,36 @@
+def fungsi_python():
+    print("hai ini adalah ara")
+
+
+
+
+
+
+nama = "Arum Rahmadhani"
+umur = "17"
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur)
+
+# List
+
+hewan = ["momo","miky","milo","moly"]
+print(hewan[3])
+
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print("penjumlahan : " + str(angka1 + angka2+ angka3 +15))
+print("pengurangan : " + str(angka3 - angka2 - angka1))
+print("pembagian : " + str(angka2 / angka1))
+print("perkalian : " + str(angka1 * angka2 * angka3))
+print("modulus : " + str(13 % 10))
+
+
+nama = input("masukkan nama anda : ")
+print("ternyata nama anda adalah " + nama)
+
+
+fungsi_python()
+# INI KOMENTAR 
+# INI BUKAN KOMENTAR 
+# ctrl + /
